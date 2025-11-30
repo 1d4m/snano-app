93ddcd2f70b916b4c3d2794834ea86f584736b47
daxchx <daxchx.v1@gmail.com>
2025-11-30 23:26:05 +0900

    feat: Textareaコンポーネントにtextareaのプロパティを追加 (#6)

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -2,7 +2,7 @@ import { Textarea as OriginalTextarea } from "@/lib/shadcn/textarea";
 
 type Props = {
   placeholder?: string;
-};
+} & React.ComponentProps<"textarea">;
 
 function Textarea({ placeholder }: Props) {
   return <OriginalTextarea placeholder={placeholder} />;
