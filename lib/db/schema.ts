03dbbbaffda7b0cc773e2b5d8ed654d92e21e18f
daxchx <daxchx.v1@gmail.com>
2025-12-07 22:11:35 +0900

    feat: プレイリストアイテムスキーマを追加 (#12)

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -5,3 +5,12 @@ export const playlists = pgTable("playlists", {
   id: serial("id").primaryKey(),
   title: varchar("title", { length: 256 }).notNull(),
 });
+
+export const playlist_items = pgTable("playlist_items", {
+  id: serial("id").primaryKey(),
+  playlist_id: text("playlist_id").notNull(),
+  title: varchar("title", { length: 256 }).notNull(),
+  description: text("description"),
+  limit_at: text("limit_at"),
+  is_completed: text("is_completed").notNull(),
+});
