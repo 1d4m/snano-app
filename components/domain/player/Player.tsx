49a65c92f1b5b556da915ac0e407e9c38213a12c
daxchx <daxchx.v1@gmail.com>
2025-12-14 22:41:09 +0900

    feat: uiを更新 (#7)

--- components/domain/player/Player.tsx
+++ components/domain/player/Player.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { playerSelector } from "@/store/slices/player";
-import { useBoundStore } from "@/store/store";
-import { playAlarmLoop } from "@/utils/alarm";
 import { Pause, Play } from "lucide-react";
 import { useEffect } from "react";
 import { useShallow } from "zustand/react/shallow";
 
-// TODO: 状態を操作するロジックはカスタムフックに記載する
+import { useBoundStore } from "@/store";
+import { playerSelector } from "@/store/slices/player";
+import { playAlarmLoop } from "@/utils/alarm";
 
 function Player() {
   const {
@@ -39,20 +38,18 @@ function Player() {
   if (!currentItem) return null;
 
   return (
-    <div className="fixed bottom-2 w-[calc(100%-16px)] h-13 mx-2 bg-[#0A0A0A] border border-[#2b2b2b] rounded-lg flex flex-col justify-between overflow-hidden">
+    <div className="fixed bottom-16 w-[calc(100%-16px)] h-13 mx-2 bg-[#0A0A0A] border border-[#2b2b2b] rounded-lg flex flex-col justify-between overflow-hidden">
       <div className="flex items-center justify-between gap-x-2 p-2">
         <div className="flex items-center gap-x-2">
           <div className="size-8 rounded bg-[#121212]"></div>
-          <div>{currentItem.title}</div>
-          <span>{count}</span>
-          <span>{currentItem.limitAt}</span>
+          <div className="text-sm">{currentItem.title}</div>
         </div>
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center pr-2">
           <button onClick={isPlaying ? pause : play}>
             {isPlaying ? (
-              <Pause className="size-4.5" />
+              <Pause className="size-4" />
             ) : (
-              <Play className="size-4.5" />
+              <Play className="size-4" />
             )}
           </button>
         </div>
