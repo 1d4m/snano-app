41c02bde7e137ed0717fda66e8edb51a2e1abf64
daxchx <daxchx.v1@gmail.com>
2026-01-04 21:25:35 +0900

    feat: ログ取得と登録にユーザー認証を追加 (#21)

--- lib/repositories/log.ts
+++ lib/repositories/log.ts
@@ -1,24 +1,38 @@
-import { desc, eq } from "drizzle-orm";
+import { desc, eq, and } from "drizzle-orm";
 
 import { db } from "@/lib/db";
-import { logs } from "@/lib/db/schema";
+import { logs, playlists } from "@/lib/db/schema";
 
 /**
  * ログを取得する
  * @param playlistId プレイリストID
  * @returns ログリスト
  */
-export async function getLogs(playlistId?: string) {
+export async function getLogs(userId: string, playlistId?: string) {
   if (playlistId) {
     return await db
       .select()
       .from(logs)
-      .where(eq(logs.playlist_id, playlistId))
+      .innerJoin(playlists, eq(logs.playlist_id, playlists.id))
+      .where(
+        and(eq(playlists.user_id, userId), eq(logs.playlist_id, playlistId))
+      )
       .orderBy(desc(logs.timestamp))
       .limit(10);
   }
 
-  return await db.select().from(logs).orderBy(desc(logs.timestamp)).limit(10);
+  // ★ playlistId 無しでも userId で絞る！
+  return await db
+    .select({
+      id: logs.id,
+      playlistId: logs.playlist_id,
+      title: logs.title,
+      timestamp: logs.timestamp,
+    })
+    .from(logs)
+    .innerJoin(playlists, eq(logs.playlist_id, playlists.id))
+    .where(eq(playlists.user_id, userId))
+    .orderBy(desc(logs.timestamp));
 }
 
 export type CreateLogInput = {
